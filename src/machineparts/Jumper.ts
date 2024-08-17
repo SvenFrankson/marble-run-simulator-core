7eb386c0747c5da95c43b725c1d3af88b7398e34
SvenFrankson <sven.frankson@gmail.com>
2024-08-17 11:36:30 +0200

    Make Jumper more sensical at 90 degrees.

--- src/machineparts/Jumper.ts
+++ src/machineparts/Jumper.ts
@@ -21,15 +21,29 @@ namespace MarbleRunSimulatorCore {
             template.nExtendable = true;
             template.xMirrorable = true;
 
-            let d = 2.5 * tileHeight;
-            let aDeg = template.n * 10;
-            let aRad = (aDeg / 180) * Math.PI;
+            if (n < 9) {
+                let d = 2.5 * tileHeight;
+                let aDeg = template.n * 10;
+                let aRad = (aDeg / 180) * Math.PI;
+    
+                let xEnd = tileWidth * 0.5 + Math.cos(aRad) * d;
+                let yEnd = -tileHeight * template.h + Math.sin(aRad) * d;
+    
+                template.trackTemplates[0] = new TrackTemplate(template);
+                template.trackTemplates[0].trackpoints = [new TrackPoint(template.trackTemplates[0], new BABYLON.Vector3(-tileWidth * 0.5, -tileHeight * template.h, 0), Tools.V3Dir(90)), new TrackPoint(template.trackTemplates[0], new BABYLON.Vector3(xEnd, yEnd, 0), Tools.V3Dir(90 - aDeg))];
+            }
+            else {
+                let d = 2.5 * tileHeight - 0.02;
 
-            let xEnd = tileWidth * 0.5 + Math.cos(aRad) * d;
-            let yEnd = -tileHeight * template.h + Math.sin(aRad) * d;
-
-            template.trackTemplates[0] = new TrackTemplate(template);
-            template.trackTemplates[0].trackpoints = [new TrackPoint(template.trackTemplates[0], new BABYLON.Vector3(-tileWidth * 0.5, -tileHeight * template.h, 0), Tools.V3Dir(90)), new TrackPoint(template.trackTemplates[0], new BABYLON.Vector3(xEnd, yEnd, 0), Tools.V3Dir(90 - aDeg))];
+                template.trackTemplates[0] = new TrackTemplate(template);
+                template.trackTemplates[0].trackpoints = [
+                    new TrackPoint(template.trackTemplates[0], new BABYLON.Vector3(-tileWidth * 0.5, -tileHeight * template.h, 0), Tools.V3Dir(90)),
+                    new TrackPoint(template.trackTemplates[0], new BABYLON.Vector3(-tileWidth * 0.5 + 0.02, -tileHeight * template.h, 0), Tools.V3Dir(90)),
+                    new TrackPoint(template.trackTemplates[0], new BABYLON.Vector3(-tileWidth * 0.5 + 0.02 + d * Math.SQRT2 / 2, tileHeight * 0.5 - 0.02 - d * Math.SQRT2 / 2, 0)),
+                    new TrackPoint(template.trackTemplates[0], new BABYLON.Vector3(tileWidth * 0.5, tileHeight * 0.5 - 0.01, 0), Tools.V3Dir(0)),
+                    new TrackPoint(template.trackTemplates[0], new BABYLON.Vector3(tileWidth * 0.5, tileHeight * 0.5, 0), Tools.V3Dir(0), Tools.V3Dir(-90))
+                ];
+            }
 
             if (mirrorX) {
                 template.mirrorXTrackPointsInPlace();
